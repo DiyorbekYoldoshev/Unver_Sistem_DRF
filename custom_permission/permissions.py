44be7493c2c737ce464dbc397683fc616cc9c2a1
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2025-10-24 11:53:29 +0500

    Permission

---
+++ custom_permission/permissions.py
@@ -0,0 +1,34 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+# 1. Faqat admin (superuser yoki staff)
+class IsAdminUserOrReadOnly(BasePermission):
+
+    """Admin yozishi mumkin boshqalar o'qiy oladi"""
+
+    def has_permission(self, request, view):
+
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+# 2. Faqat super admin (superuser=True)
+
+class IsSuperAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_superuser
+
+
+# 3. Faqat Employee uchun
+class IsEmployee(BasePermission):
+    """Employee role tekshiradi"""
+    def has_permission(self, request, view):
+        return hasattr(request.user,'role') and request.user.role == 'employee'
+
+# 4. Fataq Teacher uchun
+class IsTeacher(BasePermission):
+    """Teacher rolini tekshirdi"""
+
+    def has_permission(self, request, view):
+        return hasattr(request.user,'role') and request.user.role == 'teacher'
