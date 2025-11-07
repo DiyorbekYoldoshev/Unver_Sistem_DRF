4f9e8c628de71a2319a68b89f6be04c3f6d7eb69
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2025-11-07 19:41:58 +0500

    Initial commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django import forms
 from .models import User
 
-@admin.register(User)
+class CustomCreationForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username','role','is_active')
+
+
+
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('role',)}),
+    model = User
+    list_display = ('username', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'role')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+
+
+admin.site.register(User, CustomUserAdmin)
--- accounts/views/accounts_views.py
+++ accounts/views/accounts_views.py
@@ -1,10 +1,11 @@
-from django_filters import rest_framework as django_filters
-from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from core.middleware import CHAT_ID,TOKEN
 from ..filters import ProfileFilter, UserFilter
@@ -22,6 +23,46 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # Localized messages
+        messages = {
+            "UZ": "Siz ro'yhatdan o'tdingiz",
+            "RU": "Вы зарегистрированы",
+            "EN": "You have registered successfully",
+        }
+        success_message = messages.get(user.language, "You have registered successfully")
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                'message':success_message,
+                'user': {
+                    'id':user.id,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'role': user.role,
+                    'phone': user.phone,
+                    'gender': user.gender,
+                    'birth_date': user.birth_date,
+                    'address': user.address,
+                    'avatar': user.avatar,
+                },
+                'refresh':str(refresh),
+                'access': str(refresh.access_token)
+            },status=status.HTTP_201_CREATED
+        )
+
+
+    @action(detail=False, methods=['get', 'put'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = ProfileSerializer(request.user, data=request.data, partial=True)
+        if request.method == 'PUT':
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data)
 
     def list(self, request, *args, **kwargs):
         # GET /api/v1/user/ ga so‘rov keldi
@@ -50,12 +91,11 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
+    queryset = User.objects.select_related('user').all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = CustomPagination
     filterset_class = ProfileFilter
-
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['user__username', 'first_name', 'last_name', 'phone', 'address']
     ordering_fields = ['id', 'first_name']
