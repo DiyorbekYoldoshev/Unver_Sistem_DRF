dd7b246207ff67b6dfac5f2ea9f8b5721ade3e2e
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2025-11-11 18:34:13 +0500

    Initial commit

--- accounts/models.py
+++ accounts/models.py
@@ -5,19 +5,29 @@ from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
+
+    def create_user(self,username,password, **extra_fields):
         if not username:
-            raise ValueError("The Username must be set")
-        user = self.model(username=username, **extra_fields)
+            raise ValueError("The username must be set")
+        extra_fields.setdefault('is_staff',False)
+        extra_fields.setdefault('is_superuser',False)
+        user = self.model(username=username,**extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
+    def create_superuser(self,username,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         return self.create_user(username, password, **extra_fields)
+
 
 phone_regex = RegexValidator(
     regex=r'^\+998\d{9}$',
